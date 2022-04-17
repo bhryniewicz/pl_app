b18b753419406dc00a96be5e8eafed494240e3bd
Bartosz Hryniewicz <bartosz.hryniewicz3@gmail.com>
2022-04-17 13:03:07 +0200

    remove .env

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Header } from 'components/Header/Header';
 import { Wrapper, WidthWrapper } from './App.styles';
 import { Main } from 'components/Main/Main';
 import { Footer } from 'components/Footer/Footer';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
